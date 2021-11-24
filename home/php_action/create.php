7bfcbd8f0417a73c25c53ab04f3c55088f08ea4e
Leonardo <leo.mortari.forinn@gmail.com>
2021-11-24 13:49:29 -0300

    Alterado DB e adaptado no código

--- home/php_action/create.php
+++ home/php_action/create.php
@@ -1,26 +1,20 @@
 <?php
 session_start();
-require_once 'C:/xampp/htdocs/PositivoTurismo/db_connect.php';
-include_once 'C:/xampp/htdocs/PositivoTurismo/funcao.php';
+require_once '../../db_connect.php';
+include_once '../../funcao.php';
 
 if (isset($_POST['btn-cadastrar'])) {
-    ValidaIndex();
     $nome = mysqli_escape_string($connect, $_POST['nome']);
     $usuario = Usuario($nome);
-    $cpf = mysqli_escape_string($connect, $_POST['cpf']);
-    $email = strtolower(mysqli_escape_string($connect, $_POST['email']));
     $uf = strtoupper(mysqli_escape_string($connect, $_POST['uf']));
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $cpf = mysqli_escape_string($connect, $_POST['cpf']);
     $logradouro = mysqli_escape_string($connect, $_POST['logradouro']);
     $passaporte = mysqli_escape_string($connect, $_POST['passaporte']);
-    if ($passaporte == "") {
-        global $passaporte;
-        $passaporte = 'Não possui';
-    }
+    $email = strtolower(mysqli_escape_string($connect, $_POST['email']));
+    $idade = mysqli_escape_string($connect, $_POST['idade']);
     $senha = mysqli_escape_string($connect, md5($_POST['senha']));
-
     $sql = "INSERT INTO cliente (nome, cpf, email, uf, dataNascimento, logradouro, passaporte, usuario, senha) VALUES ('$nome', '$cpf', '$email', '$uf', '$idade', '$logradouro', '$passaporte','$usuario', '$senha')";
-
+    echo $sql;
     if (mysqli_query($connect, $sql)) {
         $_SESSION['mensagem'] = "Cadastrado com sucesso!";
         header('Location: ../index.php?sucesso');
